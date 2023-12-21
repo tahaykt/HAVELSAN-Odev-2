408dd8fc0f9b37d486cff5b39ab54bc2e0f7e205
Taha Yakıt <107004218+tahaykt@users.noreply.github.com>
2023-12-21 14:10:01 +0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+permission=$1
+user=$2
+group=$3
+file=$4
+ad=$5
+soyad=$6
+numara=$7
+
+echo "Permission:$permission User:$user Group:$group File:$file"
+echo "Ad:$ad Soyad:$soyad Numara:$numara"
